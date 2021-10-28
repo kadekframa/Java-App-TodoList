accebcbd56531dff01f99166f252a25d7fa22cec
kadekframa <dframa19@gmail.com>
2021-10-28 22:16:04 +0800

    first commit, menentukan bisnis logic

---
+++ src/AplikasiTodolist.java
@@ -0,0 +1,29 @@
+public class AplikasiTodolist {
+
+    public static String[] model = new String[10];
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    /**
+     * Menampilkan todo list
+     */
+    public static void showTodoList(){};
+
+    /**
+     * Menambahkan todo list
+     */
+    public static void addTodoList(){
+
+    };
+
+    /**
+     * Menghapus todo list
+     */
+    public static void removeTodoList(){
+
+    };
+}
